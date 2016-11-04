b37926395cd4edfd9d4775373ae62ba1cb603f33
amandanko <amandanko@yahoo.com>
2016-11-04 08:17:10 -0500

    Hello class!

--- ebook-reader/src/controller/EBookController.java
+++ ebook-reader/src/controller/EBookController.java
@@ -5,8 +5,8 @@ import view.EBookView;
 
 public class EBookController {
 
-	EBookModel model;
-	EBookView view;
+	private EBookModel model;
+	private EBookView view;
 	
 	public EBookController( EBookModel m, EBookView v ){
 		this.model = m;
